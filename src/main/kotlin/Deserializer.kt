c7952cacfaab1280b622c4ea88a82207f2dba165
garyma <garymar2007@gmail.com>
2024-08-25 21:32:05 +0200

    inline function with reified keywork for generic type in serialize/deserialize.

--- src/main/kotlin/Deserializer.kt
+++ src/main/kotlin/Deserializer.kt
@@ -1,19 +1,32 @@
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 data class Person(
     val id: String,
-    val name: String
+    val name: String,
+    val dob: String
 )
+
+private inline fun <reified T> Gson.fromJson(json: String): T {
+    return this.fromJson(json, object : TypeToken<T>() {}.type)
+}
+
+private inline fun <reified T> Gson.fromObject(obj: T): String {
+    return this.toJson(obj, object : TypeToken<T>() {}.type)
+}
 
 fun main() {
     val jsonString = """
         {
-            "id": "id",
-            "name": "name"
+            "id": "123",
+            "name": "Gary Ma",
+            "dob": "1990-01-01"
         }
     """.trimIndent()
 
-    val person = Gson().fromJson(jsonString, Person::class.java)
+    val person = Gson().fromJson<Person>(jsonString)
 
     println(person) // output: Person(id=id, name=name)
+
+    println(Gson().fromObject(person)) // output: {"id":"id","name":"name"}
 }
